b3bcfd95d27f9ab8045d508e2ce604e56e4c0c92
hadassahpenuela <hadassah.penuela-24@cpu.edu.ph>
2026-09-10 18:00:47 +0800

    Final update with logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>MY WEB APP DEPLOYMENT TASK</h1>
+  <img src="/logo.png" alt="My Logo" style="width: 200px; height: auto;" />
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
