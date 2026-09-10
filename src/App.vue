6845e742a96289d1a3fbbb995bf4911f5e7ecfad
hadassahpenuela <hadassah.penuela-24@cpu.edu.ph>
2026-09-10 18:37:03 +0800

    Centered Layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
-<script setup></script>
-
 <template>
-  <h1>My Web App Deployment Task</h1>
-  <img src="/logo.png" alt="My Logo" style="width: 200px; height: auto;" />
+  <div class="container">
+    <h1>My Web App Deployment Task</h1>
+    <img src="/logo.png" alt="My Logo" />
+  </div>
 </template>
 
 <style scoped>
 .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;    
+  justify-content: center; 
+  min-height: 80vh;        
   text-align: center;
-  margin-top: 50px;
 }
+
 h1 {
-  color:purple
+  color: purple;
+  margin-bottom: 30px;     
 }
+
 img {
   width: 200px;
   height: auto;
-  margin-top: 20px;
 }
 </style>
